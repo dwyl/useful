8465e5d805f63b54f55329b42803b3fc51455324
nelsonic <nelson+github@dwyl.io>
2020-10-01 12:32:56 +0100

    add standard dev dependencies for building packages

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Useful.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +28,29 @@ defmodule Useful.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      # Keep Code Tidy: https://github.com/rrrene/credo
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+
+      # track test coverage: https://github.com/parroty/excoveralls
+      {:excoveralls, "~> 0.13.2", only: [:test, :dev]},
+
+      # Create Documentation Hex.docs: https://hex.pm/packages/ex_doc
+      {:ex_doc, "~> 0.22.6", only: :dev},
+
+      # Git pre-commit hook: https://github.com/dwyl/elixir-pre-commit
+      {:pre_commit, "~> 0.3.4", only: :dev}
+    ]
+  end
+
+  # package info for publishing to Hex.pm
+  defp package() do
+    [
+      files: ~w(lib LICENSE mix.exs README.md),
+      name: "useful",
+      licenses: ["GNU GPL v2.0"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/dwyl/useful"}
     ]
   end
 end
