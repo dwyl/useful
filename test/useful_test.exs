31b5c3c18d68a33d1659aec44bfad26e05632ab1
nelsonic <nelson+github@dwyl.io>
2020-10-04 00:24:15 +0100

    add tests for flatten_map/1 #2

--- test/useful_test.exs
+++ test/useful_test.exs
@@ -14,4 +14,51 @@ defmodule UsefulTest do
     expected = Useful.atomize_map_keys(map)
     assert expected == %{id: 1, name: "alex", nested: %{age: 17, height: 185}}
   end
+
+  test "flatten_map/1 flattens a deeply nested map" do
+    map = %{name: "alex", data: %{age: 17, height: 185}}
+    expected = Useful.flatten_map(map)
+    assert expected == %{"name" => "alex", "data.age" => 17, "data.height" => 185}
+  end
+
+  test "flatten_map/1 handles Date and DateTime values" do
+    map = %{
+      date: Date.from_erl!({2000, 1, 1}),
+      data: %{
+        dateTime: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"),
+        height: 185}
+      }
+    expected = Useful.flatten_map(map)
+    assert expected == %{
+      "date" => ~D[2000-01-01],
+      "data.dateTime" => ~U[2016-05-24 13:26:08.003Z],
+      "data.height" => 185
+    }
+  end
+
+  test "flatten super nested map" do
+    map = %{
+      name: "Alex",
+      data: %{
+        address: %{
+          first_line: "1600 Pennsylvania Avenue",
+          post_code: "20500",
+          detail: %{
+            house_color: "white",
+            more_info: %{
+              "architect" => "James Hoban"
+            }
+          }
+        }
+      }
+    }
+    expected = Useful.flatten_map(map)
+    assert expected == %{
+      "data.address.detail.house_color" => "white",
+      "data.address.detail.more_info.architect" => "James Hoban",
+      "data.address.first_line" => "1600 Pennsylvania Avenue",
+      "data.address.post_code" => "20500",
+      "name" => "Alex"
+    }
+  end
 end
